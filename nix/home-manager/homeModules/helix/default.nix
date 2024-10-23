0c409c39146f385ec24f0c58901cbb9667e98d77
Adam Engebretson <adam@enge.me>
2024-10-22 22:21:31 -0500

    More typescript formatting

--- nix/home-manager/homeModules/helix/default.nix
+++ nix/home-manager/homeModules/helix/default.nix
@@ -2,7 +2,45 @@
   pkgs,
   unstable,
   ...
-}: {
+}: let
+  eslint-ls = with unstable;
+    buildNpmPackage rec {
+      pname = "vscode-langservers-extracted";
+      version = "4.8.0";
+
+      src = fetchFromGitHub {
+        owner = "hrsh7th";
+        repo = pname;
+        rev = "v${version}";
+        hash = "sha256-sGnxmEQ0J74zNbhRpsgF/cYoXwn4jh9yBVjk6UiUdK0=";
+      };
+
+      npmDepsHash = "sha256-LFWC87Ahvjf2moijayFze1Jk0TmTc7rOUd/s489PHro=";
+
+      buildPhase = let
+        extensions =
+          if stdenv.isDarwin
+          then "${vscodium}/Applications/VSCodium.app/Contents/Resources/app/extensions"
+          else "${vscodium}/lib/vscode/resources/app/extensions";
+      in ''
+        npx babel ${extensions}/css-language-features/server/dist/node \
+          --out-dir lib/css-language-server/node/
+        npx babel ${extensions}/html-language-features/server/dist/node \
+          --out-dir lib/html-language-server/node/
+        npx babel ${extensions}/json-language-features/server/dist/node \
+          --out-dir lib/json-language-server/node/
+        cp -r ${vscode-extensions.dbaeumer.vscode-eslint}/share/vscode/extensions/dbaeumer.vscode-eslint/server/out \
+          lib/eslint-language-server
+      '';
+
+      meta = with lib; {
+        description = "HTML/CSS/JSON/ESLint language servers extracted from vscode";
+        homepage = "https://github.com/hrsh7th/vscode-langservers-extracted";
+        license = licenses.mit;
+        maintainers = with maintainers; [lord-valen];
+      };
+    };
+in {
   programs.helix = {
     enable = true;
     package = unstable.helix;
@@ -12,10 +50,12 @@
       gopls
       deadnix
       alejandra
+      eslint-ls
       terraform-ls
       lua-language-server
       yaml-language-server
       unstable.vue-language-server
+      unstable.nodePackages.prettier
       nodePackages.typescript-language-server
     ];
     settings = {
@@ -68,6 +108,24 @@
             command = "alejandra";
           };
           language-servers = ["nil" "statix" "deadnix"];
+        }
+        {
+          name = "typescript";
+          auto-format = true;
+          language-servers = ["typescript-language-server" "vscode-eslint-language-server"];
+          formatter = {
+            command = "prettier";
+            args = ["--parser" "typescript"];
+          };
+        }
+        {
+          name = "tsx";
+          auto-format = true;
+          language-servers = ["typescript-language-server" "vscode-eslint-language-server"];
+          formatter = {
+            command = "prettier";
+            args = ["--parser" "typescript"];
+          };
         }
         {
           name = "vue";
