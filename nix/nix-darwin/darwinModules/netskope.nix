93e3308f6211798451710b8b4b31080cc40e7fb9
Adam Engebretson <adam@enge.me>
2025-02-13 16:07:36 -0600

    Disabling Nix SSL for now

--- nix/nix-darwin/darwinModules/netskope.nix
+++ nix/nix-darwin/darwinModules/netskope.nix
@@ -10,13 +10,14 @@
   bundlePath = "${cabundle}/etc/ssl/certs/ca-bundle.crt";
 in {
   nix = {
-    extraOptions = ''
-      ssl-cert-file = ${bundlePath}
-    '';
+    # extraOptions = ''
+    #   ssl-cert-file = ${bundlePath}
+    # '';
   };
 
   home-manager.users.${username}.home.sessionVariables = {
     AWS_CA_BUNDLE = bundlePath;
+    CA_BUNDLE_PATH = bundlePath;
     NETSKOPE_CA_BUNDLE = bundlePath;
     NODE_EXTRA_CA_CERTS = bundlePath;
   };
